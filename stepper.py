339f5634ad155a9a7f9a6cc7bdd247685d68870c
gwjoseph <geo22joseph@gmail.com>
2020-05-08 20:55:37 -0400

    stepper.py file cleanup and comments added

--- stepper.py
+++ stepper.py
@@ -10,30 +10,28 @@ yAxisMotorPins = (31,33,35,37)
 yAxisSeqCounter = 0
 
 
-seq =   [
-        [1,0,0,0],
+seq = [[1,0,0,0],
         [1,1,0,0],
         [0,1,0,0],
         [0,1,1,0],
         [0,0,1,0],
         [0,0,1,1],
         [0,0,0,1],
-        [1,0,0,1]
-        ]
+        [1,0,0,1]]
 
-
+#setup the gpio pins for both motors
 def setup():
-    GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering
+    GPIO.setmode(GPIO.BOARD)
     for pin in xAxisMotorPins:
         GPIO.setup(pin,GPIO.OUT)
         
     for pin in yAxisMotorPins:
         GPIO.setup(pin,GPIO.OUT)
 
+# takes a single half step for the X axis motor and keep track of the GPIO pin output sequence
 def stepOneXAxis(direction,ms):
     global xAxisSeqCounter
     for pin in range (len(xAxisMotorPins)):
-
         if(seq[xAxisSeqCounter][pin] == 1):
             GPIO.output(xAxisMotorPins[pin], True)
         else:
@@ -49,10 +47,10 @@ def stepOneXAxis(direction,ms):
         ms = 3
     time.sleep(ms*0.001)
 
+# takes a single half step for the Y axis motor and keep track of the GPIO pin output sequence
 def stepOneYAxis(direction,ms):
     global yAxisSeqCounter
     for pin in range (len(yAxisMotorPins)):
-
         if(seq[yAxisSeqCounter][pin] == 1):
             GPIO.output(yAxisMotorPins[pin], True)
         else:
@@ -70,32 +68,44 @@ def stepOneYAxis(direction,ms):
 
 
 # continuous rotation function, the parameter steps specifies the rotation cycles, every four steps is a cycle
-def moveSteps(direction, ms, steps,axis):
+def moveSteps(direction, ms, steps, axis):
+    # have the axis check on the outside of the loop so you don't do the axis check done on each step loop.
     if(axis == "x"):
         for i in range(steps):
             stepOneXAxis(direction, ms)
-    else:
+    elif(axis == "y"):
         for i in range(steps):
             stepOneYAxis(direction, ms)
             
-        
+            
+#TODO:  see about having the motors actually turn 1 degree and not 
+# method to make the x axis motor turn right        
 def moveRight():
    moveSteps(1,3,11,"x") #11.3777 is a single degree
     
+# method to make the x axis motor turn left  
 def moveLeft():
     moveSteps(-1,3,11,"x") #11.3777 is a single degree
-        
+
+# method to make the y axis motor turn up  
 def moveUp():
    moveSteps(1,3,11,"y") #11.3777 is a single degree
-    
+
+# method to make the y axis motor turn down  
 def moveDown():
     moveSteps(-1,3,11,"y") #11.3777 is a single degree
         
-# function used to stop motor
-def motorStop():
-    for i in range(0,4,1):
-        GPIO.output(motorPins[i],GPIO.LOW)
-            
+# function used to stop the x axis motor
+def stopXAxisMotor():
+    for i in range(len(xAxisMotorPins)):
+        GPIO.output(xAxisMotorPins[i],GPIO.LOW)
+        
+# function used to stop the y axis motor
+def stopXAxisMotor():
+    for i in range(len(yAxisMotorPins)):
+        GPIO.output(yAxisMotorPins[i],GPIO.LOW)
+
+#loop method that will keep an open while loop turning the motors back and forth roughly 360 degrees
 def loop():
     while True:
         for i in range (360):
@@ -107,7 +117,7 @@ def loop():
             moveUp()
         time.sleep(0.5)
 
-
+# method that will listen for terminal input
 def userInputLoop():
     while True:
         text = raw_input()
@@ -117,7 +127,7 @@ def userInputLoop():
             moveRight()
 
 def destroy():
-    GPIO.cleanup()             # Release resource
+    GPIO.cleanup()  # Release resource
     
 if __name__ == '__main__': # Program entrance
      print ('Program is starting...')
@@ -127,6 +137,5 @@ if __name__ == '__main__': # Program entrance
         #userInputLoop()
      except KeyboardInterrupt:  # Press ctrl-c to end the program.
          print ('exiting...')
-         #destroy() #leaving this here incase I want to use this for something, but the finally block is handling the destroy
      finally:
         destroy()
